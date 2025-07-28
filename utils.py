733e9a50a9bd02c36c6f00f01bf89756dba3c1ba
Gu1306 <gustavo@coutorosa.com>
2025-07-28 06:54:32 -0300

    tils.py

--- utils.py
+++ utils.py
@@ -1,15 +1,31 @@
+import os
 import requests
-from config import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
 
 def exchange_token(code):
+    client_id = os.getenv("CLIENT_ID")
+    client_secret = os.getenv("CLIENT_SECRET")
+    redirect_uri = os.getenv("REDIRECT_URI")
+
+    print("Trocando token com:")
+    print("client_id:", client_id)
+    print("client_secret:", "[OCULTO]")
+    print("code:", code)
+    print("redirect_uri:", redirect_uri)
+
     response = requests.post(
         "https://www.strava.com/oauth/token",
         data={
-            "client_id": CLIENT_ID,
-            "client_secret": CLIENT_SECRET,
+            "client_id": client_id,
+            "client_secret": client_secret,
             "code": code,
             "grant_type": "authorization_code",
-            "redirect_uri": REDIRECT_URI,
+            "redirect_uri": redirect_uri
         },
+        timeout=10
     )
+
+    print("Resposta do Strava:", response.status_code)
+    print("Body:", response.text)
+
+    response.raise_for_status()
     return response.json()
