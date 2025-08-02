26d13cd5b87938929b3baa25e6f3ba201d1f104e
Gustavo Rosa <gustavo@coutorosa.com>
2025-08-02 14:56:21 -0300

    Corrige chamada de save_athlete passando o dicionário completo

--- app.py
+++ app.py
@@ -98,10 +98,20 @@ def ver_atividades(token):
                 nome_atleta = f"{html.escape(resultado['firstname'])}_{html.escape(resultado['lastname'])}".replace(" ", "_") if resultado else "atleta"
                 athlete_id = resultado['athlete_id'] if resultado else None
 
-        txt_content = "\n\n".join([str(a) for a in atividades])
-        dados_codificados = base64.b64encode(txt_content.encode("utf-8")).decode("utf-8")
-        data_atual = datetime.datetime.now().strftime("%d-%m-%Y")
-        nome_arquivo = f"{nome_atleta}_treinos_{data_atual}.txt"
+      # Divide as atividades em duas páginas
+atividade_strs = [str(a) for a in atividades]
+txt_1_200 = "\n\n".join(atividade_strs[:200])
+txt_201_400 = "\n\n".join(atividade_strs[200:400])
+
+# Codifica os arquivos
+dados_1_200 = base64.b64encode(txt_1_200.encode("utf-8")).decode("utf-8")
+dados_201_400 = base64.b64encode(txt_201_400.encode("utf-8")).decode("utf-8")
+
+# Data e nomes dos arquivos
+data_atual = datetime.datetime.now().strftime("%d-%m-%Y")
+nome_1_200 = f"{nome_atleta}_treinos_1_a_200_{data_atual}.txt"
+nome_201_400 = f"{nome_atleta}_treinos_201_a_400_{data_atual}.txt"
+
 
         limite_data = datetime.datetime.now() - datetime.timedelta(days=90)
         atividades_90dias = [a for a in atividades if datetime.datetime.strptime(a['start_date_local'][:10], "%Y-%m-%d") >= limite_data]
